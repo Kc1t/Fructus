9bf6189a33a93c11ec204227ee35458e3fdf6010
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2024-09-07 17:51:54 -0300

    Fructus Landing Page

---
+++ fructus-website/src/app/page.tsx
@@ -0,0 +1,62 @@
+// Components
+import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
+
+// Assets
+import { DownloadCloud } from "lucide-react";
+
+export default function Page() {
+  return (
+    <div className="bg-[#FDF1F5] w-full min-h-screen tv:h-auto overflow-x-hidden flex flex-col justify-between tv:justify-center items-center">
+      <div className="h-[70vh] tv:h-[60vh] w-full overflow-hidden flex flex-col justify-between items-center">
+        <Navbar />
+        <div className="text-center flex flex-col lg:gap-2 z-[10] w-full">
+          <div className="my-3">
+            <span className="bg-[#FFF0DC] text-[#FB9333] p-2 px-4 rounded-full font-semibold">
+              Software feito em C
+            </span>
+          </div>
+
+          <h1 className="text-2xl font-bold tracking-wide sm:text-5xl text-[#191919] md:text-5xl">
+            <span className="block xl:inline">
+              <span className="mx-1 text-[rgb(251,147,51)]">Administre</span>
+              suas <br /> operações com Fructus
+            </span>
+          </h1>
+          <p className="mx-auto mt-3 max-w-4xl text-sm w-5/6 md:w-full lg:text-xl text-[#252525] sm:mt-5 md:mt-2">
+            Fructus é um software em C para um hortifruti fictício, desenvolvido
+            no PIM II. Este site foi criado para baixar os arquivos e oferecer
+            uma visão mais detalhada do projeto.
+          </p>
+          <div className="my-2">
+            <Button className="bg-[#252525] text-sm md:text-base text-white hover:bg-[#252525]/80 rounded-full px-6 py-6 md:py-7">
+              Baixar Repositório{" "}
+              <div className="bg-white mx-2 rounded-full h-8 w-8 flex items-center justify-center">
+                <DownloadCloud className="text-black w-4" />
+              </div>{" "}
+            </Button>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div className="my-4 px-2 w-full max-w-6xl flex items-center justify-between flex-col">
+        <div className="bg-[#F5F5F5] w-full h-8 md:h-12 flex justify-center md:justify-between items-center px-4">
+          <div className="hidden md:flex gap-1 items-center justify-center">
+            <div className="h-4 w-4 bg-[#D83B3B] rounded-full"></div>
+            <div className="h-4 w-4 bg-[#E2C423] rounded-full"></div>
+            <div className="h-4 w-4 bg-[#03CA0B] rounded-full"></div>
+          </div>
+          <div className="font-semibold text-xs">
+            hortifruti@fructus — 80 x 24
+          </div>
+          <div></div>
+        </div>
+        <video
+          className=""
+          controls
+          src="https://cdn-cf-east.streamable.com/video/mp4/fgi0fq.mp4?Expires=1726000988287&amp;Key-Pair-Id=APKAIEYUVEN4EVB2OKEQ&amp;Signature=eYnlhXp-OXDsFA9N6fMSn4fqHydG-pgsrhVw3XW0wrv6YrM35kudgSbuPNyrUDu5jNc-6FmckBx~6j4~JEl6QLm0p9Ea45fETY5UzOHpmQRycZxaRZRaqhbIPgwf7EdJ-P8O3bgcttQYH6nnuVTmwbpicdhV00q9LfRYPn7ZwI7S0cNFxHXfg4MCvbAu0lQ86UP7HWqZ4Zmz6gq8ZprDKTZ~jpKAavZhoesKFUaVR5-Z3IyrYjhw2MfwQ~mnHa7z~gCEFW-OSIkGo3VieJpND5ewHdoOQtF2H0XnVzgGVWZZR1N14lhniQ479~cSuFBvUiECbqp5wlQibNkZ8hu~Dg__"
+        ></video>
+      </div>
+    </div>
+  );
+}
---
+++ fructus-website/src/components/Navbar.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+// Modules
+import * as React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+// Components
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+// Assets
+import Logo from "@/assets/Logo.png";
+import { DownloadCloud, Github, Menu } from "lucide-react";
+
+const navItems = [
+  { title: "Home", href: "/" },
+  { title: "Sobre", href: "/sobre" },
+  { title: "Download", href: "/download" },
+];
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <nav className="py-4 w-full z-[10]">
+      <div className="flex items-center justify-between px-4 mx-auto w-[90%] lg:w-[70%] tv:max-w-6xl sm:px-6 lg:px-8">
+        <div className="flex items-center flex-shrink-0">
+          <Link href="/" className="text-xl font-bold">
+            <Image src={Logo} className="w-14" alt="Logo" />
+          </Link>
+        </div>
+
+        <div className="hidden md:block bg-sub-dark rounded-2xl">
+          <div className="px-6 py-2">
+            {/* {navItems.map((item) => (
+              <React.Fragment key={item.title}>
+                <Link
+                  href={item.href}
+                  className="px-3 py-2 text-sm font-semibold rounded-xl text-[#575757]  hover:border-black/70 hover:text-black"
+                >
+                  {item.title}
+                </Link>
+              </React.Fragment>
+            ))} */}
+          </div>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <a
+            href="https://github.com/Kc1t/desafio-verzel"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="z-20 items-center justify-center bg-[#FB9333] hover:bg-[#FB9333]/80 transition-all ease-in-out duration-300 h-11 w-11 md:h-10 md:w-10 cursor-pointer rounded-full hidden md:flex"
+          >
+            <Github className="text-white w-5 h-5" />
+          </a>
+
+          <Link href="/download">
+            <Button className="hidden md:flex px-6 gap-2 rounded-full bg-[#FB9333] text-white hover:bg-[#FB9333]/80 font-medium">
+              <DownloadCloud className="w-5 text-white" />
+              Baixar
+            </Button>
+          </Link>
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
+            <SheetTrigger asChild>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="md:hidden text-black"
+              >
+                <Menu className="h-6 w-6 text-black" />
+                <span className="sr-only">Fechar</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent
+              side="right"
+              className="bg-white text-black border-black/20 border-l-2"
+            >
+              <div className="flex flex-col space-y-4 mt-4">
+                {navItems.map((item) => (
+                  <Link
+                    key={item.title}
+                    href={item.href}
+                    className="px-3 py-2 text-sm font-semibold text-black"
+                    onClick={() => setIsOpen(false)}
+                  >
+                    {item.title}
+                  </Link>
+                ))}
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </nav>
+  );
+}
